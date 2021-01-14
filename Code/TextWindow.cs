ae1f036fd654c926204e599cd341a6f9828d56c8
Jan Vermeer <70522775+Verm0158@users.noreply.github.com>
2021-01-14 14:49:53 +0100

    Add files via upload

---
+++ Code/TextWindow.cs
@@ -0,0 +1,53 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+
+public class TextWindow : MonoBehaviour {
+
+    private Text upperText;
+    private Text lowerText;
+
+    private void Awake() {
+        upperText = transform.Find("UpperText").GetComponent<Text>();
+        lowerText = transform.Find("LowerText").GetComponent<Text>();
+
+        transform.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+    }
+
+    private void Start() {
+        Bird.GetInstance().Answers += Bird_Answers;
+
+        Hide();
+    }
+
+    private void Bird_Answers(object sender, System.EventArgs e) {
+        upperText.text = Level.GetInstance().upperAnswer();
+  
+        lowerText.text = Level.GetInstance().lowerAnswer();
+
+        Show();
+    }
+
+    public void Hide() {
+        gameObject.SetActive(false);
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+
+}
